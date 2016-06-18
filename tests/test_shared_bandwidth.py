c7a5247d24aada4cc66d633e98d7ead0b7f11a6f
Brett Viren <brett.viren@gmail.com>
2016-06-18 08:03:49 -0400

    Add working preemption.

--- tests/test_shared_bandwidth.py
+++ tests/test_shared_bandwidth.py
@@ -6,13 +6,30 @@ Simulate a comm link with bandwidth shared by multiple streams.
 import simpy
 
 class Stream(object):
-    def __init__(self, ident, size, now):
+    def __init__(self, ident, size, now, bandwidth=0):
         self.ident = ident
         self.size = size
         self.remaining = size
         self.start = now
+        self.bandwidth = bandwidth
+
+    def update(self, elapsed):
+        "Update remaining assuming elapsed time."
+        self.remaining -= self.bandwidth*elapsed
+
+    @property
+    def eta(self):
+        "Return ETA at current bandwidth."
+        return self.remaining / self.bandwidth
+
+    def __lt__(self, other):
+        if self.remaining < other.remaining: return True
+        if self.size < other.size: return True
+        if self.start < other.start: return True
+        return id(self) < id(other)
+
     def __str__(self):
-        return 'stream "%s" start at %d, remaining: %.1f/%.1f (%.2f%%)' % (self.ident, self.start, self.remaining, self.size, 100.0*self.remaining/self.size)
+        return 'stream "%s" start at %d, remaining: %.1f/%.1f (%.2f%%) bw=%.1f' % (self.ident, self.start, self.remaining, self.size, 100.0*self.remaining/self.size, self.bandwidth)
 
 
 class Link(object):
@@ -32,40 +49,76 @@ class Link(object):
             print ('accept with %d ongoing:  %s' % (len(self.streams), stream))
             self.env.process(self.deliver(stream))
 
-    def deliver(self, stream):
+    def stage(self, stream):
+        "Stage a stream"
         self.streams[stream.ident] = stream
         nstreams = len(self.streams)
-        bandwidth = self.bandwidth / nstreams
-        eta = stream.remaining / bandwidth
-        prio = -1*nstreams
-        with self.transmit.request(priority=prio) as req:
+        bw = self.bandwidth / nstreams
+        for s in self.streams.values():
+            s.bandwidth = bw
+        print ('staged with bw=%.1f and %d streams: %s' % (bw, nstreams, stream))
+
+    def unstage(self, stream):
+        self.streams.pop(stream.ident)
+        nstreams = len(self.streams)
+        print ('unstaged with %d streams: %s' % (nstreams, stream))
+        if not nstreams:
+            return
+        bw = self.bandwidth / nstreams
+        for s in self.streams.values():
+            s.bandwidth = bw
+
+    def find_next(self):
+        "Return stream with smallest ETA"
+        if not self.streams:
+            return None
+        etas = [(s.eta,s) for s in self.streams.values()]
+        etas.sort()
+        print ('\n'.join(["%.1f: %s" % (t,s) for t,s in etas]))
+        return etas[0][1]
+
+    def update(self, elapsed):
+        "Update all streams after elapsed"
+        for s in self.streams.values():
+            s.update(elapsed)
+            print ('\tupdated: %s' % str(s))
+        return
+
+    def deliver(self, stream):
+        prio = -1*len(self.streams)
+        print ('GRAB with priority %d' % prio)
+        with self.transmit.request(priority=prio) as req: # this prempts any existing delivery
             yield req
+            self.stage(stream)
+            stream = self.find_next()
             start = self.env.now
+            print ('delivering t=%.1f prio=%d: %s' % (start, prio, stream))
             try:
-                print ('delivery with prio %d, bw %.1f, eta in %.1f: %s' % (prio, bandwidth, eta, stream))
-                yield self.env.timeout(eta)
-            except simpy.Interrupt as interrupt:
-                # update stream state
-                #dt = self.env.now - interrupt.cause.usage_since
-                dt = self.env.now - start
-                stream.remaining -= dt*bandwidth
-                self.streams.pop(stream.ident)
-                print ('interupted at %.1f after %.1f with %s' % (self.env.now, dt, stream))
-                self.inbox.put(stream)
+                yield self.env.timeout(stream.eta)
+            except simpy.Interrupt: # someone else came to deliver
+                print ('INTERUPTED %s' % str(stream))
+                self.update(self.env.now-start)
                 return
 
-            dt = self.env.now - start
-            stream.remaining -= dt*bandwidth
-            self.streams.pop(stream.ident)
-            print ('delivered %s' % stream)
+            # reach here, successfully waited out a stream to finish
+            elapsed = self.env.now - start
+            self.update(elapsed)
+            print ('delivered after %.1f: %s' % (elapsed, stream))
+            self.unstage(stream)
             yield self.outbox.put(stream)
+
+        stream = self.find_next()
+        print ('moving on to: %s' % stream)
+        if stream:
+            self.env.process(self.deliver(stream))
+
 
 def slurp(env, link):
     while True:
         stream = yield link.outbox.get()
         print ('final: %s' % stream)
         dt = env.now-stream.start
-        print ('\ttook %.1f avg bandwidth=%f' % (dt, stream.size/dt))
+        print ('\tDONE: took %.1f avg bandwidth=%f' % (dt, stream.size/dt))
 
 def add_stream(env, link, wait, ident, size):
     print ('waiting %f to start stream "%s"' % (wait, ident))
@@ -82,7 +135,7 @@ def test_it():
     env.process(add_stream(env, link, 2, 'b', 50))
     env.process(add_stream(env, link, 4, 'c', 50))
 
-    env.run(until=100)
+    env.run(until=200)
 
 if '__main__' == __name__:
     test_it()
